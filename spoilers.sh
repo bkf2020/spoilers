d7ce7a1b53ca3a46e09a2665aeb1ef20bf2a9be0
bkf2020 <74793713+bkf2020@users.noreply.github.com>
2021-08-02 21:07:55 -0700

    adding text to speech option for annoucements

--- spoilers.sh
+++ spoilers.sh
@@ -1,15 +1,26 @@
 #!/bin/sh
+T2S="yes"
 
 dialog --msgbox "Welcome to spoilers! This tool will give you 2 Pomodoro rounds of 50 minutes work and 10 minutes rest." 10 60
 dialog --msgbox "When you are working, the internet will be completely turned off. If you need internet to submit a problem, press ENTER in the dialog box to select the 'I need to submit' button and you will get a two minute timer to submit." 10 60
 dialog --yesno "Note that you will need NetworkManager installed. Do you have this installed? If not, install it and relaunch this script." 10 60 || exit
+dialog --defaultno --yesno "Do you want to turn on text to speech? It will update you on when a work session starts/finishes or when a break starts/finishes.\nNote this requires you have 'espeak' installed." 10 60 || T2S="no"
 dialog --yesno "Once you are ready to start the first work session, press 'Yes'. You can press 'No' if you don't want to continue" 10 60 || exit
-clear
 submittimer() {
 	dialog --infobox "Waiting for NetworkManager to turn on internet. Don't worry: the timer hasn't started. Please be patient." 10 60
 	nmcli networking on
+	if [ $T2S = "yes" ]
+	then
+		dialog --title "Annoucement" --infobox "This submission timer will start now" 10 60
+		espeak "The submission timer will start now" 2> /dev/null
+	fi
 	dialog --timeout 60 --msgbox "Submit your solution within the next two minutes" 10 60
 	dialog --timeout 60 --msgbox "Submit your solution within the next minute" 10 60
+	if [ $T2S = "yes" ]
+	then
+		dialog --title "Annoucement" --infobox "This submission timer is over. Back to the work session." 10 60
+		espeak "The submission timer is over. Back to the work session." 2> /dev/null
+	fi
 }
 
 worktimer() {
@@ -42,6 +53,19 @@ breaktimer() {
 
 for i in $(seq 2)
 do
+	if [ $T2S = "yes" ]
+	then
+		dialog --title "Annoucement" --infobox "The work session is starting" 10 60
+		espeak "The work session is starting" 2> /dev/null
+	fi
 	worktimer
+	if [ $T2S = "yes" ]
+	then
+		dialog --title "Annoucement" --infobox "The work is over. The break session is starting" 10 60
+		espeak "The work is over. The break session is starting" 2> /dev/null
+	fi
 	breaktimer
 done
+
+dialog --msgbox "Thanks for using spoilers! You can start the script again if you want." 10 60
+clear
